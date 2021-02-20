ee69d4be26624a9809e7d949f80f50777e367efb
Keshav98 <keshav.mishra@mechademy.com>
2021-02-20 14:02:03 +0530

    Remove unnecessary code

--- model.py
+++ model.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import numpy  as np
-import matplotlib.pyplot as plt
 import pickle
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 import load_data
-
-plt.rcParams['figure.figsize'] = 25, 8
-plt.rcParams['axes.grid'] = True
-plt.style.use('fivethirtyeight')
 
 X, y = load_data.create_feature_target()
 reg=DecisionTreeRegressor(max_depth=5)
 reg.fit(X,y)
 y_pred=reg.predict(X)
 print(reg.score(X, y))
+
+pickle.dump(reg, open('model/prof.pkl', 'wb'))
